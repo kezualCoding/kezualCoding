ea28cbeddcd62c6bf42b401a40fe7d19dc6cc8f6
Vaiditya2207 <vaidityatanwar2207@gmail.com>
2024-03-23 19:55:12 +0530

    toggle theme draft - 1

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,17 +1,35 @@
-let isDark = true;
+let isDark = false;
 
 function themeChanger(){
     if (isDark){
         isDark = !isDark;
+        document.querySelector('.mainNav').classList.remove('mainNavDark');
+        document.body.style.background = 'white';
+        document.body.querySelector('.main-page').style.color = 'black';
+        document.body.querySelector('.mainHeading').style.backgroundImage = 'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)';
+        document.body.querySelector('.mainHeading__description').style.color = 'black';
+        document.body.querySelector('.cta').style.color = 'black';
+        document.body.querySelector('.cta').style.border = '1px solid black';
+        document.body.querySelector('.mainHeading__title').style.color = '#637498';
+        document.body.querySelector('.mainHeading__title').style.textShadow = '0 0 0px black';
+        document.body.querySelector('.mainHeading__preTitle').style.color = '#637498';
+        document.body.querySelector('.mainHeading__preTitle').style.textShadow = '0 0 0px black';
+        document.body.querySelector('.stay-tuned').style.color = 'silver';
     }
     else{
         isDark = !isDark;
         document.querySelector('.mainNav').classList.add('mainNavDark');
-        document.body.style.background = "transparent url('assets/images/sparkle.jpeg')";
-        document.body.style.backgroundImage = 'linear-gradient(to top, #0c2458 0%, #040a19 50%)';
+        document.body.style.background = 'linear-gradient(to top, #070e18 0%, black 100%)';
         document.body.querySelector('.main-page').style.color = 'silver';
-        document.body.querySelector('.mainHeading').style.backgroundImage = 'linear-gradient(to top, #040a19 0%, black 50%)';
-        document.body.querySelector('.mainHeading__description').style.color = 'silver';
+        document.body.querySelector('.mainHeading').style.backgroundImage = "url('assets/images/sky.jpg')";
+        document.body.querySelector('.mainHeading__description').style.color = 'black';
+        document.body.querySelector('.cta').style.color = 'black';
+        document.body.querySelector('.cta').style.border = '1px solid black';
+        document.body.querySelector('.mainHeading__title').style.color = 'black';
+        document.body.querySelector('.mainHeading__title').style.textShadow = '0 0 10px black';
+        document.body.querySelector('.mainHeading__preTitle').style.color = 'black';
+        document.body.querySelector('.mainHeading__preTitle').style.textShadow = '0 0 10px black';
+        document.body.querySelector('.stay-tuned').style.color = 'silver';
      }
 }
 
