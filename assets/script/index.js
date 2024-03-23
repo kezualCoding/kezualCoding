d5f2aa3ce5dd9c70309a9768f4a473c4e1197a8d
Vaiditya2207 <vaidityatanwar2207@gmail.com>
2024-03-23 17:23:10 +0530

    theme changer prototype

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,21 @@
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
+let isDark = true;
+
+function themeChanger(){
+    if (isDark){
+        isDark = !isDark;
+    }
+    else{
+        isDark = !isDark;
+        document.querySelector('.mainNav').classList.add('mainNavDark');
+        document.body.style.background = "transparent url('assets/images/sparkle.jpeg')";
+        document.body.style.backgroundImage = 'linear-gradient(to top, #0c2458 0%, #040a19 50%)';
+        document.body.querySelector('.main-page').style.color = 'silver';
+        document.body.querySelector('.mainHeading').style.backgroundImage = 'linear-gradient(to top, #040a19 0%, black 50%)';
+        document.body.querySelector('.mainHeading__description').style.color = 'silver';
+     }
+}
+
+document.getElementById('themeChanger').addEventListener('click', function(){
+    themeChanger();
+})
+
